6d92e791a4e2175dfab39713db90c7d92a5bcc11
Nitkarsh Chourasia <{playnitkarsh@gmail.com}+{NitkarshChourasia}@users.noreply.github.com>
2022-10-10 20:06:54 +0530

    Database Uploading

--- exampleTest.sql
+++
@@ -1,7 +0,0 @@
-SHOW DATABASES;
-CREATE DATABASES dbName;
-USE dbName;
-CREATE TABLE exampleTableOne (First VARCHAR, Second INT, Third INT);
-CREATE TABLE exampleTableTwo (First VARCHAR, Second INT, Third INT);
-CREATE TABLE exampleTableThree (First VARCHAR, Second INT, Third INT);
-SHOW TABLES;
