ef96ae7e770cd97a0594a07769925b2ff3a8f61a
Nitkarsh Chourasia <{playnitkarsh@gmail.com}+{NitkarshChourasia}@users.noreply.github.com>
2022-10-10 13:28:29 +0530

    Backup

---
+++ exampleTest.sql
@@ -0,0 +1,7 @@
+SHOW DATABASES;
+CREATE DATABASES dbName;
+USE dbName;
+CREATE TABLE exampleTableOne (First VARCHAR, Second INT, Third INT);
+CREATE TABLE exampleTableTwo (First VARCHAR, Second INT, Third INT);
+CREATE TABLE exampleTableThree (First VARCHAR, Second INT, Third INT);
+SHOW TABLES;
