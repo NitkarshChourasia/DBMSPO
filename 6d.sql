0f3ec5cbe2bb026df1f3c602f6da57d035b6df0a
Purshotam Bohra <67221507+PBJI@users.noreply.github.com>
2022-10-10 23:53:06 +0530

    .... stilll going on.

---
+++ 6d.sql
@@ -0,0 +1,129 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: company1
+-- ------------------------------------------------------
+-- Server version	5.7.9-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `empdepartment`
+--
+
+DROP TABLE IF EXISTS `empdepartment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `empdepartment` (
+  `EmpNo` int(11) NOT NULL,
+  `EmpDepart` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`EmpNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empdepartment`
+--
+
+LOCK TABLES `empdepartment` WRITE;
+/*!40000 ALTER TABLE `empdepartment` DISABLE KEYS */;
+INSERT INTO `empdepartment` VALUES (101,'computer'),(102,'computer'),(103,'civil'),(104,'ETC'),(105,'Electrical'),(106,'IT'),(107,'Transport'),(108,'IT');
+/*!40000 ALTER TABLE `empdepartment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employee` (
+  `EmpNo` int(11) NOT NULL,
+  `EmpName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `City` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Salary` int(11) DEFAULT NULL,
+  `JoinDate` date DEFAULT NULL,
+  PRIMARY KEY (`EmpNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (101,'Rajesh','Pune',15000,'2022-10-10'),(102,'Vedant','Mumbai',25000,'2022-10-10'),(103,'Swati','Mumbai',15000,'2022-10-10'),(104,'Smar','Nagpur',28000,'2022-10-10'),(105,'Swaraj','Virar',16000,'2022-10-10'),(106,'Nitkarsh','Virar',70000,'2022-10-10'),(107,'Pallavi','Virar',45000,'2022-10-10'),(108,'Purshotam','Bhinmal',90000,'2022-10-10');
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productdetails`
+--
+
+DROP TABLE IF EXISTS `productdetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `productdetails` (
+  `ProductID` int(11) DEFAULT NULL,
+  `ProductName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Quantity` int(11) DEFAULT NULL,
+  `Price` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productdetails`
+--
+
+LOCK TABLES `productdetails` WRITE;
+/*!40000 ALTER TABLE `productdetails` DISABLE KEYS */;
+INSERT INTO `productdetails` VALUES (1001,'Pendrive',100,900),(1002,'Hardisk',200,4000),(1003,'Headphone',1000,15000),(1004,'DVD',20,1000),(1005,'Speaker',600,2400),(1006,'SSD',300,7000),(1007,'Monitor',300,9000),(1008,'Mouse',900,400);
+/*!40000 ALTER TABLE `productdetails` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `saledetails`
+--
+
+DROP TABLE IF EXISTS `saledetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `saledetails` (
+  `SaleNo` int(11) unsigned DEFAULT NULL,
+  `ProductID` int(11) DEFAULT NULL,
+  `Quantity` int(11) unsigned DEFAULT NULL,
+  `Price` int(11) unsigned DEFAULT NULL,
+  `CustomerName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `saledetails`
+--
+
+LOCK TABLES `saledetails` WRITE;
+/*!40000 ALTER TABLE `saledetails` DISABLE KEYS */;
+INSERT INTO `saledetails` VALUES (2001,1001,50,900,'savni'),(2002,1004,10,1000,'savni'),(2003,1003,120,15000,'savni'),(2004,1005,420,2400,'Rohan'),(2005,1002,40,4000,'Akash');
+/*!40000 ALTER TABLE `saledetails` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-10 23:48:58
